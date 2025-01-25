f9a002ca4a988b5ca81f12c7dc11a8042ce21026
Pawel Pawlowski <ppawlowski@zignago.com>
2025-01-25 12:36:40 +0100

    inicjalizacja projektu Spring Boot

---
+++ src/main/java/com/n1hoo/przepisy/PrzepisyApplication.java
@@ -0,0 +1,13 @@
+package com.n1hoo.przepisy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrzepisyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrzepisyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/n1hoo/przepisy/PrzepisyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n1hoo.przepisy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrzepisyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
