84999aa1eaee67c0783c13a39a7c1e42a5eb2f43
MatSz-pro <pajper27@gmail.com>
2025-01-30 19:44:16 +0100

    Debugowanie 2

--- src/main/java/com/n1hoo/przepisy/model/Recipe.java
+++ src/main/java/com/n1hoo/przepisy/model/Recipe.java
@@ -2,6 +2,7 @@ package com.n1hoo.przepisy.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
